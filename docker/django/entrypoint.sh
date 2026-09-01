709ded58e6f265714c06c5fc0546199afa028b47
Ronny Rentner <github@ronny-rentner.de>
2026-09-01 15:50:50 +0200

    Add the site's Docker deployment, adapted from Relonee for shared proxy and postgres

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Waiting for postgres..."
+
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python manage.py migrate
+
+python manage.py db_worker &
+
+exec "$@"
---
+++ secrets/readme.md
@@ -0,0 +1,2 @@
+This folder is intended for secret files like 3rd party API keys etc. used by Django.
+It will be mounted into Docker under `/app/secrets` and the contents will not be committed.

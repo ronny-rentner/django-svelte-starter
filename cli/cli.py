7d354f952979615ffa11cc3fa355f3292c47c1b8
Ronny Rentner <github@ronny-rentner.de>
2026-09-05 08:15:50 +0200

    Take COMPOSE_PROJECT_NAME from the env file instead of computing it

--- cli/cli.py
+++ cli/cli.py
@@ -126,9 +126,6 @@ class DockerCommand:
         # Build the base Docker Compose command
         self._dc_cmd = self._build_dc_cmd()
 
-        #TODO: REMOVE THIS LINE, ITS REALLY REALLY WRONG
-        os.environ["COMPOSE_PROJECT_NAME"] = PROJECT_ROOT.name.replace(".", "-")
-
         os.environ["COMPOSE_MENU"] = "0"
         #os.environ["LINES"] = click.output.run_command('tput lines').stdout.strip()
         #os.environ["COLUMNS"] = click.output.run_command('tput cols').stdout.strip()
